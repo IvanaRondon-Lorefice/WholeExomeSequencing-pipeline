c96069e6a7ae5bee77cce0c7ab48bba33d67dd7c
Ivana Rondon Lorefice <43853639+IvanaRondon-Lorefice@users.noreply.github.com>
2026-08-20 12:49:54 +0200

    Update 00_Interval_List.sh

--- Scripts/00_Interval_List.sh
+++ Scripts/00_Interval_List.sh
@@ -10,9 +10,10 @@
 # For this library, we need to use the GRCm38. 
 #---------------------------------------------------------------------------------#
 
-source /opt/ohpc/pub/apps/anaconda3/cic-env
-conda activate /vols/GPArkaitz_bigdata/DATA_shared/NewCluster_Software/conda_envs/WESenv
+source "$CONDA_ACTIVATE"
+conda activate "$CONDA_ENV_WES"
 
-picard BedToIntervalList I=/vols/GPArkaitz_bigdata/irondon/AC-82_WESmouse/Twist_Mouse_Exome_Target_Rev1_7APR20_Chr-Modified.bed \
-    O=/vols/GPArkaitz_bigdata/irondon/AC-82_WESmouse/Twist_Mouse_Exome.interval_list \
-    SD=/vols/GPArkaitz_bigdata/DATA_shared/Genomes_Rocky/Sequences/Mus_musculus.GRCm38.dna.primary_assembly.dict
+picard BedToIntervalList \
+    I="${BASE_DIR}/reference/Twist_Mouse_Exome_Target.bed" \
+    O="${BASE_DIR}/reference/Twist_Mouse_Exome.interval_list" \
+    SD="${DATA_SHARED}/${GENOME_DICT}"
